457c6d795994921013a36d4c697415bbefceabeb
golab97 <pesto2330@gmail.com>
2022-04-15 20:31:04 +0200

    Add project files.

---
+++ FrameFinder/Finder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameFinder
+{
+    public class Finder
+    {
+        readonly Queue<byte> _rawDataQueue;
+
+        public Finder()
+        {
+            _rawDataQueue = new();
+        }
+        public void AddToQueue(byte data)
+        {
+            _rawDataQueue.Enqueue(data);
+        }
+        ~Finder() 
+        {
+            _rawDataQueue.Clear();
+        }
+    }
+}
